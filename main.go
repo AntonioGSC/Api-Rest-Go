fbb4a72aa8510844ea6a9aa86bf06d4e54783353
AntonioGSC <antoniogabrielagsc@gmail.com>
2022-04-12 22:46:54 -0300

    starting project

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Home Page")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", Home)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
+
+func main() {
+	fmt.Println("Iniciando o servidor Rest com Go")
+
+	HandleRequest()
 }
